1b025d79df01ae512d6ff80dcec8252552d14770
rubtrin <149677005+rubtrin@users.noreply.github.com>
2023-12-26 18:25:51 +0300

    clockChanges
    ch
    
    clock
    
    clockChanges
    
    clockChanges
    
    now the clock stops twice

---
+++ index.js
@@ -0,0 +1,23 @@
+let timerId;
+
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', function () {
+    if (!timerId) {
+        timerId = setInterval(updateClock, 1000); 
+    }
+});
+
+const stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', function () {
+    clearInterval(timerId);
+    timerId = null;
+});
+
+function updateClock() {
+    const clock = document.getElementById('clock');
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    clock.textContent = hours + ':' + minutes + ':' + seconds;
+}
